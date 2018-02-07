af0180b581f9cfbe6370f935e85cfd571ebe1bec
kiatkumjorn <kiatkumjorn2@gmail.com>
2018-02-07 16:33:51 +0700

    Add leftComponent

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,12 +4,14 @@ import { NgModule } from '@angular/core';
 import { AppComponent } from './app.component';
 import { SidebarComponent } from './sidebar/sidebar.component';
 import { HeaderComponent } from './shared/header/header.component';
+import { LeftComponent } from './left/left.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     SidebarComponent,
-    HeaderComponent
+    HeaderComponent,
+    LeftComponent
   ],
   imports: [
     BrowserModule
